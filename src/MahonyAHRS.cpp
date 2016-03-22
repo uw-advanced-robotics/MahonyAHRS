e532e20084bcba07ff742e63b2eef4937883907e
PaulStoffregen <paul@pjrc.com>
2016-03-22 04:19:38 -0700

    Delete redundant variables & clean up comments

--- src/MahonyAHRS.cpp
+++ src/MahonyAHRS.cpp
@@ -1,6 +1,6 @@
-//=====================================================================================================
+//=============================================================================================
 // MahonyAHRS.c
-//=====================================================================================================
+//=============================================================================================
 //
 // Madgwick's implementation of Mayhony's AHRS algorithm.
 // See: http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/
@@ -16,37 +16,30 @@
 // Algorithm paper:
 // http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=4608934&url=http%3A%2F%2Fieeexplore.ieee.org%2Fstamp%2Fstamp.jsp%3Ftp%3D%26arnumber%3D4608934
 //
-//=====================================================================================================
+//=============================================================================================
 
-//---------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
 // Header files
 
 #include "MahonyAHRS.h"
 #include <math.h>
 
-//---------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
 // Definitions
 
 #define sampleFreq	512.0f		// sample frequency in Hz
 #define twoKpDef	(2.0f * 0.5f)	// 2 * proportional gain
 #define twoKiDef	(2.0f * 0.0f)	// 2 * integral gain
 
-//---------------------------------------------------------------------------------------------------
-// Variable definitions
 
-volatile float twoKp = twoKpDef;											// 2 * proportional gain (Kp)
-volatile float twoKi = twoKiDef;											// 2 * integral gain (Ki)
-volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;					// quaternion of sensor frame relative to auxiliary frame
-volatile float integralFBx = 0.0f,  integralFBy = 0.0f, integralFBz = 0.0f;	// integral error terms scaled by Ki
-
-
-//====================================================================================================
+//============================================================================================
 // Functions
 
-//---------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
 // AHRS algorithm update
 
-Mahony::Mahony() {
+Mahony::Mahony()
+{
 	twoKp = twoKpDef;	// 2 * proportional gain (Kp)
 	twoKi = twoKiDef;	// 2 * integral gain (Ki)
 	q0 = 1.0f;
@@ -58,7 +51,8 @@ Mahony::Mahony() {
 	integralFBz = 0.0f;
 }
 
-void Mahony::update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz) {
+void Mahony::update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
+{
 	float recipNorm;
 	float q0q0, q0q1, q0q2, q0q3, q1q1, q1q2, q1q3, q2q2, q2q3, q3q3;
 	float hx, hy, bx, bz;
@@ -66,13 +60,15 @@ void Mahony::update(float gx, float gy, float gz, float ax, float ay, float az, 
 	float halfex, halfey, halfez;
 	float qa, qb, qc;
 
-	// Use IMU algorithm if magnetometer measurement invalid (avoids NaN in magnetometer normalisation)
+	// Use IMU algorithm if magnetometer measurement invalid
+	// (avoids NaN in magnetometer normalisation)
 	if((mx == 0.0f) && (my == 0.0f) && (mz == 0.0f)) {
 		updateIMU(gx, gy, gz, ax, ay, az);
 		return;
 	}
 
-	// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+	// Compute feedback only if accelerometer measurement valid
+	// (avoids NaN in accelerometer normalisation)
 	if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
 
 		// Normalise accelerometer measurement
@@ -87,23 +83,23 @@ void Mahony::update(float gx, float gy, float gz, float ax, float ay, float az, 
 		my *= recipNorm;
 		mz *= recipNorm;
 
-        // Auxiliary variables to avoid repeated arithmetic
-        q0q0 = q0 * q0;
-        q0q1 = q0 * q1;
-        q0q2 = q0 * q2;
-        q0q3 = q0 * q3;
-        q1q1 = q1 * q1;
-        q1q2 = q1 * q2;
-        q1q3 = q1 * q3;
-        q2q2 = q2 * q2;
-        q2q3 = q2 * q3;
-        q3q3 = q3 * q3;
+		// Auxiliary variables to avoid repeated arithmetic
+		q0q0 = q0 * q0;
+		q0q1 = q0 * q1;
+		q0q2 = q0 * q2;
+		q0q3 = q0 * q3;
+		q1q1 = q1 * q1;
+		q1q2 = q1 * q2;
+		q1q3 = q1 * q3;
+		q2q2 = q2 * q2;
+		q2q3 = q2 * q3;
+		q3q3 = q3 * q3;
 
-        // Reference direction of Earth's magnetic field
-        hx = 2.0f * (mx * (0.5f - q2q2 - q3q3) + my * (q1q2 - q0q3) + mz * (q1q3 + q0q2));
-        hy = 2.0f * (mx * (q1q2 + q0q3) + my * (0.5f - q1q1 - q3q3) + mz * (q2q3 - q0q1));
-        bx = sqrtf(hx * hx + hy * hy);
-        bz = 2.0f * (mx * (q1q3 - q0q2) + my * (q2q3 + q0q1) + mz * (0.5f - q1q1 - q2q2));
+		// Reference direction of Earth's magnetic field
+		hx = 2.0f * (mx * (0.5f - q2q2 - q3q3) + my * (q1q2 - q0q3) + mz * (q1q3 + q0q2));
+		hy = 2.0f * (mx * (q1q2 + q0q3) + my * (0.5f - q1q1 - q3q3) + mz * (q2q3 - q0q1));
+		bx = sqrtf(hx * hx + hy * hy);
+		bz = 2.0f * (mx * (q1q3 - q0q2) + my * (q2q3 + q0q1) + mz * (0.5f - q1q1 - q2q2));
 
 		// Estimated direction of gravity and magnetic field
 		halfvx = q1q3 - q0q2;
@@ -113,21 +109,22 @@ void Mahony::update(float gx, float gy, float gz, float ax, float ay, float az, 
 		halfwy = bx * (q1q2 - q0q3) + bz * (q0q1 + q2q3);
 		halfwz = bx * (q0q2 + q1q3) + bz * (0.5f - q1q1 - q2q2);
 
-		// Error is sum of cross product between estimated direction and measured direction of field vectors
+		// Error is sum of cross product between estimated direction
+		// and measured direction of field vectors
 		halfex = (ay * halfvz - az * halfvy) + (my * halfwz - mz * halfwy);
 		halfey = (az * halfvx - ax * halfvz) + (mz * halfwx - mx * halfwz);
 		halfez = (ax * halfvy - ay * halfvx) + (mx * halfwy - my * halfwx);
 
 		// Compute and apply integral feedback if enabled
 		if(twoKi > 0.0f) {
-			integralFBx += twoKi * halfex * (1.0f / sampleFreq);	// integral error scaled by Ki
+			// integral error scaled by Ki
+			integralFBx += twoKi * halfex * (1.0f / sampleFreq);
 			integralFBy += twoKi * halfey * (1.0f / sampleFreq);
 			integralFBz += twoKi * halfez * (1.0f / sampleFreq);
 			gx += integralFBx;	// apply integral feedback
 			gy += integralFBy;
 			gz += integralFBz;
-		}
-		else {
+		} else {
 			integralFBx = 0.0f;	// prevent integral windup
 			integralFBy = 0.0f;
 			integralFBz = 0.0f;
@@ -159,16 +156,18 @@ void Mahony::update(float gx, float gy, float gz, float ax, float ay, float az, 
 	q3 *= recipNorm;
 }
 
-//---------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
 // IMU algorithm update
 
-void Mahony::updateIMU(float gx, float gy, float gz, float ax, float ay, float az) {
+void Mahony::updateIMU(float gx, float gy, float gz, float ax, float ay, float az)
+{
 	float recipNorm;
 	float halfvx, halfvy, halfvz;
 	float halfex, halfey, halfez;
 	float qa, qb, qc;
 
-	// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+	// Compute feedback only if accelerometer measurement valid
+	// (avoids NaN in accelerometer normalisation)
 	if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
 
 		// Normalise accelerometer measurement
@@ -177,26 +176,27 @@ void Mahony::updateIMU(float gx, float gy, float gz, float ax, float ay, float a
 		ay *= recipNorm;
 		az *= recipNorm;
 
-		// Estimated direction of gravity and vector perpendicular to magnetic flux
+		// Estimated direction of gravity
 		halfvx = q1 * q3 - q0 * q2;
 		halfvy = q0 * q1 + q2 * q3;
 		halfvz = q0 * q0 - 0.5f + q3 * q3;
 
-		// Error is sum of cross product between estimated and measured direction of gravity
+		// Error is sum of cross product between estimated
+		// and measured direction of gravity
 		halfex = (ay * halfvz - az * halfvy);
 		halfey = (az * halfvx - ax * halfvz);
 		halfez = (ax * halfvy - ay * halfvx);
 
 		// Compute and apply integral feedback if enabled
 		if(twoKi > 0.0f) {
-			integralFBx += twoKi * halfex * (1.0f / sampleFreq);	// integral error scaled by Ki
+			// integral error scaled by Ki
+			integralFBx += twoKi * halfex * (1.0f / sampleFreq);
 			integralFBy += twoKi * halfey * (1.0f / sampleFreq);
 			integralFBz += twoKi * halfez * (1.0f / sampleFreq);
 			gx += integralFBx;	// apply integral feedback
 			gy += integralFBy;
 			gz += integralFBz;
-		}
-		else {
+		} else {
 			integralFBx = 0.0f;	// prevent integral windup
 			integralFBy = 0.0f;
 			integralFBz = 0.0f;
@@ -228,11 +228,12 @@ void Mahony::updateIMU(float gx, float gy, float gz, float ax, float ay, float a
 	q3 *= recipNorm;
 }
 
-//---------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
 // Fast inverse square-root
 // See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
 
-float Mahony::invSqrt(float x) {
+float Mahony::invSqrt(float x)
+{
 	float halfx = 0.5f * x;
 	float y = x;
 	long i = *(long*)&y;
@@ -243,6 +244,6 @@ float Mahony::invSqrt(float x) {
 	return y;
 }
 
-//====================================================================================================
+//============================================================================================
 // END OF CODE
-//====================================================================================================
+//============================================================================================
