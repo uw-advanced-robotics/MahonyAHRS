ba4f4553756a4362da58f9bddaf51803afa30736
PaulStoffregen <paul@pjrc.com>
2016-03-23 17:51:00 -0700

    Fix roll, pitch to proper intrinsic rotations

--- src/MahonyAHRS.h
+++ src/MahonyAHRS.h
@@ -1,6 +1,6 @@
-//=====================================================================================================
+//=============================================================================================
 // MahonyAHRS.h
-//=====================================================================================================
+//=============================================================================================
 //
 // Madgwick's implementation of Mayhony's AHRS algorithm.
 // See: http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/
@@ -9,12 +9,12 @@
 // 29/09/2011	SOH Madgwick    Initial release
 // 02/10/2011	SOH Madgwick	Optimised for reduced CPU load
 //
-//=====================================================================================================
+//=============================================================================================
 #ifndef MahonyAHRS_h
 #define MahonyAHRS_h
 #include <math.h>
 
-//----------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
 // Variable declaration
 
 class Mahony {
@@ -26,7 +26,7 @@ private:
 	float invSampleFreq;
 	static float invSqrt(float x);
 
-//---------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
 // Function declarations
 
 public:
@@ -34,12 +34,22 @@ public:
 	void begin(float sampleFrequency) { invSampleFreq = 1.0f / sampleFrequency; }
 	void update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
 	void updateIMU(float gx, float gy, float gz, float ax, float ay, float az);
-	float getPitch(){return atan2f(2.0f * q2 * q3 - 2.0f * q0 * q1, 2.0f * q0 * q0 + 2.0f * q3 * q3 - 1.0f);};
-	float getRoll(){return -1.0f * asinf(2.0f * q1 * q3 + 2.0f * q0 * q2);};
-	float getYaw(){return atan2f(2.0f * q1 * q2 - 2.0f * q0 * q3, 2.0f * q0 * q0 + 2.0f * q1 * q1 - 1.0f);};
+	float getPitch() {
+		float x = 2.0f * (q1*q3 - q0*q2);
+		float y = 2.0f * (q0*q1 + q2*q3);
+		float z = q0*q0 - q1*q1 - q2*q2 + q3*q3;
+		return atan2f(x, sqrtf(y*y + z*z));
+	}
+	float getRoll() {
+		float x = 2.0f * (q1*q3 - q0*q2);
+		float y = 2.0f * (q0*q1 + q2*q3);
+		float z = q0*q0 - q1*q1 - q2*q2 + q3*q3;
+		return atan2f(y, sqrtf(x*x + z*z));
+	}
+	float getYaw() {
+		return atan2f(2.0f*q1*q2 - 2.0f*q0*q3,
+			2.0f*q0*q0 + 2.0f*q1*q1 - 1.0f);
+	}
 };
 
 #endif
-//=====================================================================================================
-// End of file
-//=====================================================================================================
